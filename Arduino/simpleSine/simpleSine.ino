15e57c1ca700a4a2e40555a42422c273da59b805
Michael Harris <mharris@visgence.com>
2015-05-13 15:19:27 -0600

    updates naming of slave select pins for clairty

--- Arduino/simpleSine/simpleSine.ino
+++ Arduino/simpleSine/simpleSine.ino
@@ -4,38 +4,47 @@
 ///2014 Visgence Inc
 /////////////////////////////////////////////////////////////////////////////////
 #include <SPI.h>
-#define SS0 (1 << 2) //Slave Select 0 PORTB
-#define SS0PORT &PORTB   
+#define SS1 (1 << 2) //Slave Select 1 PORTB
+#define SS1PORT &PORTB   
 
-#define SS1 (1 << 1) //Slave Select 1 PORTB
-#define SS1PORT &PORTB
-
-#define SS2 (1 << 0) //Slave Select 2 PORTB
+#define SS2 (1 << 1) //Slave Select 2 PORTB
 #define SS2PORT &PORTB
 
-#define SS3 (1 << 7) //Slave Select 2 PORTD
-#define SS3PORT &PORTD
+#define SS3 (1 << 0) //Slave Select 3 PORTB
+#define SS3PORT &PORTB
+
+#define SS4 (1 << 7) //Slave Select 4 PORTD
+#define SS4PORT &PORTD
+
+const int ss1 = 10;
+const int ss2 = 9;
+const int ss3 = 8;
+const int ss4 = 7;
 
 void setup() {
-  // set the slaveSelectPin as an output:
+  // set the slaveSelectPins as an output:
+  pinMode (ss1, OUTPUT);
+  pinMode (ss2, OUTPUT);
+  pinMode (ss3, OUTPUT);
+  pinMode (ss4, OUTPUT);
 
-  pinMode (10, OUTPUT);
-  pinMode (11, OUTPUT);
   // initialize SPI:
   SPI.begin();
   SPI.setDataMode(SPI_MODE0);
 }
 
 int i;
-void loop() {
-  
-  for(i=0;i<0x4096;i++) {
-     //Digital Write
-     //writeMCP492x((int)(sin(2.0 * 3.14159 * double(i)/double(4095)) *(4096/2) + (4096/2)),10);
-     
-     //DriectPort  
-     writeMCP492x((int)(sin(2.0 * 3.14159 * double(i)/double(4095)) *(4096/2) + (4096/2)),SS0,SS0PORT);
+int value;
 
+void loop() {
+  for(i=0;i<0x4096;i++) {
+    value = (int)(sin(2.0 * 3.14159 * double(i)/double(4095)) *(4096/2) + (4096/2));
+    
+    //Digital Write
+    //writeMCP492x((int)(sin(2.0 * 3.14159 * double(i)/double(4095)) *(4096/2) + (4096/2)),10);
+     
+    //DriectPort
+    writeMCP492x(value, SS2, SS2PORT);
   }
 }
 
